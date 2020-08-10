d020e6162bf944ce51944d5581af36f31e09856e
Saud M. Khoja <65879129+saudmjd@users.noreply.github.com>
2020-08-10 16:41:04 -0400

    Updates

---
+++ precache-manifest.af0f28d620748d99117844f87185e41b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005422928921119f083ff06698218ef4",
+    "url": "/saudKhojaProjectFive/index.html"
+  },
+  {
+    "revision": "4379f0ef3e01a37a9a75",
+    "url": "/saudKhojaProjectFive/static/css/main.e10109ee.chunk.css"
+  },
+  {
+    "revision": "bae882ccc510a9aae3ad",
+    "url": "/saudKhojaProjectFive/static/js/2.f1df4ebc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saudKhojaProjectFive/static/js/2.f1df4ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4379f0ef3e01a37a9a75",
+    "url": "/saudKhojaProjectFive/static/js/main.23632558.chunk.js"
+  },
+  {
+    "revision": "00a3756324fa129b9306",
+    "url": "/saudKhojaProjectFive/static/js/runtime-main.60217862.js"
+  }
+]);
